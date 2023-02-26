0caa03a4664750c67ef0e7773d076bdb920a8e1a
lateofrederick <lateofrederick@gmail.com>
2023-02-26 13:27:32 +0000

    get cron to work

--- private/server.js
+++ private/server.js
@@ -1,3 +1,4 @@
+const cron = require('node-cron');
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -22,6 +23,18 @@ app.use(express.json());
 
 //Route middleware
 app.use("", routes);
+
+cron.schedule('*/15 * * * *', function() {
+  console.log('Running cron job');
+  // call the services function you want to be executed
+  // every fifteen minutes.
+  // so assuming you want to call the [fetchReports] from
+  // report.service.js, just import it above and
+  // call it fetchReports() here. it should work.
+  // if you want to change the time from 15 minutes to 10
+  // do so from the first argument and change 15 to 10 leaving
+  // the rest as it is.
+});
 
 if (process.env.NODE_ENV !== "test") {
   app.listen(process.env.PORT || 3001, () =>
