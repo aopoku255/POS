99123c0df9a64498ae7b50782713ac79c51a282b
unknown <aopoku255gmail.com>
2023-02-28 09:43:16 +0000

    pulling from Obar

--- private/database/mongodb.js
+++ private/database/mongodb.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const User = require("../schemas/User");
+require("dotenv").config()
 mongoose.set("strictQuery", false);
-const localURI = "mongodb://localhost";
 const onlineURI =
   "mongodb+srv://tekdevisal:Frimpong2010@cluster0.0dlsllz.mongodb.net/?retryWrites=true&w=majority";
 
 module.exports = (db_name) => {
   return mongoose.connect(
-    `${localURI}/${db_name}`,
+    `${onlineURI}/${db_name}`,
     () => {
       console.log("Local MongoDB Connection Successful");
     },
--- private/server.js
+++ private/server.js
@@ -6,13 +6,43 @@ const app = express();
 const keys = require("../keys.json");
 
 app.set("keys", keys.tekdevisalPOS);
-
 const routes = require("../routes/v1/routes");
-
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const User = require("./schemas/User");
+const onlineURI =
+  "mongodb+srv://tekdevisal:Frimpong2010@cluster0.0dlsllz.mongodb.net/?retryWrites=true&w=majority/tekdevisalPOS";
+const localURI = "mongodb://localhost/tekdevisalPOS";
 // const mongoose = require("./database/mongodb")(app.get("keys").db_name);
 
-require("./database/mongodb")(app.get("keys").db_name);
+// require("./database/mongodb")(app.get("keys").db_name);
+// mongoose.connect(
+//   `${localURI}/tekdevisalPOS`,
+//   { useNewUrlParser: true, useUnifiedTopology: true },
+//   () => console.log("Local Mongodb connected")
+// );
 
+mongoose.connect(
+  `${localURI}`,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("Local mongodb connected")
+);
+
+async function fetchUser() {
+  const results = await User.find();
+  console.log("Local", results);
+}
+
+fetchUser();
+setInterval(async () => {
+  mongoose.connect(
+    onlineURI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log("ONLINE mongodb connected")
+  );
+  const result = await User.find();
+  console.log("ONLINE", result[0]);
+}, 5000);
 
 // Using CORS
 app.use(cors());
